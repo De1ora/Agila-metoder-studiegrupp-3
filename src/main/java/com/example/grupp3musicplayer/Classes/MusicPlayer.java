0c85dfe75b9c497e20e61d4d9b3ae56d92cad174
PavtheCodes <pavlostheodoropouloss@gmail.com>
2024-11-22 04:04:52 +0200

    init base classes

---
+++ src/main/java/com/example/grupp3musicplayer/Classes/MusicPlayer.java
@@ -0,0 +1,10 @@
+package com.example.grupp3musicplayer.Classes;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.*;
+import javafx.scene.media.MediaPlayer;
+public class MusicPlayer {
+    private MediaPlayer mediaPlayer;
+    private Playlist playlistManager;
+}
---
+++ src/main/java/com/example/grupp3musicplayer/Classes/Playlist.java
@@ -0,0 +1,22 @@
+package com.example.grupp3musicplayer.Classes;
+
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<Track> tracks;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.tracks = new ArrayList<>();
+    }
+    public void addTrack(Track track) {
+        tracks.add(track);
+    }
+    public void removeTrack(Track track) {
+        tracks.remove(track);
+    }
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+}
