753d4f3acd5cf098dca7c6ba5671fb9b527d21bb
PavtheCodes <pavlostheodoropouloss@gmail.com>
2024-11-22 04:05:44 +0200

    test låt med path resource låten

--- src/main/java/com/example/grupp3musicplayer/HelloApplication.java
+++ src/main/java/com/example/grupp3musicplayer/HelloApplication.java
@@ -3,8 +3,11 @@ package com.example.grupp3musicplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -13,6 +16,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+
+        /* String path = getClass().getResource("/com/musik/Song.mp3").toExternalForm();
+        Media media = new Media(path);
+        MediaPlayer mediaplayer = new MediaPlayer(media);
+        mediaplayer.setAutoPlay(true); */
         stage.setScene(scene);
         stage.show();
     }
