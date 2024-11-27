3e37ed4c71b2cb128b75ff3faca33b61ac95fb3f
Lisa <lisafalth05@gmail.com>
2024-11-27 15:39:27 +0100

    test

--- Grupp3MusicPlayer/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.grupp3musicplayer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.grupp3musicplayer to javafx.fxml;
-    exports com.example.grupp3musicplayer;
-}
